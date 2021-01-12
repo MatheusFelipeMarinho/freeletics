3eef0c4a797bddfcd576ec4dcb7e77feec1e762d
MatheusFelipeMarinho <matheus_fellip@hotmail.com>
2021-01-12 08:36:36 -0300

    commit init

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import * as Location from 'expo-location';
+import MapView,  { Marker, Polyline }  from 'react-native-maps'
+
 
 export default function App() {
+  const [location, setLocation] = useState([0, 0]);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+
+      const { latitude, longitude } = location.coords;
+
+      setLocation([latitude, longitude]);     
+
+      const coordsPoli = [latitude, longitude]
+
+      console.log(coordsPoli)
+    
+    })();
+  }, [location]);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+       style={styles.map}
+       showsUserLocation={true}
+       loadingEnabled={true}
+       initialRegion={{
+        latitude: location[0],
+        longitude: location[1],
+        latitudeDelta: 0.014,
+        longitudeDelta: 0.014,
+       }}
+       >
+          <Marker       
+            style={styles.mapMarker}
+            coordinate={{
+              latitude: location[0],
+              longitude: location[1],
+            }}
+            >   
+            <View style={{backgroundColor: "red", padding: 10}}/>
+              
+          </Marker>
+          <Polyline
+		coordinates={this.coordsPoli}	strokeColor="#fb2587"	strokeWidth={10}
+	/>
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  map:{
+    flex: 1
   },
+  container: {
+    flex: 1,   
+  },
+  mapMarker: {
+    width: 90,
+    height: 80, 
+  },
+ 
 });
